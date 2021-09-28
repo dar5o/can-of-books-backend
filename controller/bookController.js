893247dd6dd0057594965b500a2fbab920af2f56
Jacob Gregor <67080552+JacobGregor@users.noreply.github.com>
2021-09-28 14:32:31 -0700

    schema corrected

--- controller/bookController.js
+++ controller/bookController.js
@@ -1,15 +1,14 @@
 'use strict';
 
 
-const { query } = require('express');
-const booksModel = require('../modules/books');
+const Books = require('../modules/books');
 
 async function getBooks (request, response) {
     const email = {};
     if (request.query.email) {
       email.email = request.query.email;
     }
-    const sendBooks = await booksModel.find(email);
+    const sendBooks = await Books.find(email);
     response.send(sendBooks);
 }
 
--- modules/books.js
+++ modules/books.js
@@ -4,40 +4,24 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const bookSchema = new Schema({
-  Books: Array,
+  title: String,
+  description: String,
+  status: String,
+  email: String,
 })
 
-const booksModel = mongoose.model('books', bookSchema);
+const Books = mongoose.model('books', bookSchema);
 
 let seedFunction = () => {
 
-  let newbooks = new booksModel({
-    Books:[{
-      title: "Unconditional Preaching of the Elves (Elven religion and pantheon)",
-      description: "This book is a written body of teachings of a specific religious organization. This book is leafed through, but otherwise good condition. It is written in Common Speech. It is part of a series of 10 volumes.",
-      status: "available",
-      email: 'vitortedario@gmail.com',
-      id:1
-
-    },
-    {
-      title: "Great Properties of Performing",
-      description: "This book is meant to teach a person a specific skill. This could be used as a permanent or temporary stat increase if read and studied. This book is leafed through, but otherwise good condition. It is written in Common Speech. It is embossed in large letters on cover.",
+  let newbooks = new Books({
+      title: "Monty Python",
+      description: "brits",
       status: "notavailable",
-      email: 'vitortedario@gmail.com',
-      id:2
-    },
-    {
-      title: "Library Book of Libraries: A photo book showing a series of decrepit bookshelves, along with locations.",
-      description: "This book is a little more eye-catching than most other mundane books. This book is leafed through, but otherwise good condition. It is written in Common Speech. It is a very thin book.",
-      status: "notavailable",
-      email: 'jacob.gregor@outlook.com',
-      id:3
-      
-    },]
-  })
+      email: 'bryce@me.com',
+    });
   newbooks.save()
   console.log(newbooks)
 }
 seedFunction();
-module.exports = booksModel
+module.exports = Books 
